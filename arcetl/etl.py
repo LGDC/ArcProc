e57341a7004e61a6253317648a918050e605e47e
Jacob Blair <jblair@lcog.org>
2016-06-13 17:35:55 -0700

    Add ability to run have ArcETL().transform's main argument be a function, as well as a string of the function name.
    Future: Would like to deprecate the string name option.

--- arcetl/etl.py
+++ arcetl/etl.py
@@ -1,7 +1,8 @@
 # -*- coding=utf-8 -*-
 """ETL objects."""
-import inspect
 import logging
+
+import funcsigs
 
 from . import helpers, metadata, operations
 
@@ -13,7 +14,7 @@ class ArcETL(object):
     """Manages a single Arc-style ETL process."""
 
     def __init__(self, name=None):
-        self.name = name if name else 'unnamed ETL'
+        self.name = name if name else 'Unnamed ETL'
         self.transform_path = None
         LOG.info("Initialized ArcETL instance for %s.", self.name)
 
@@ -35,28 +36,21 @@ class ArcETL(object):
 
     def extract(self, extract_path, extract_where_sql=None, schema_only=False):
         """Extract features to transform workspace."""
-        meta = {'description': "Extract {}.".format(extract_path)}
-        helpers.log_line('start', meta['description'])
-        # Remove previously-extant transform dataset.
-        if all([self.transform_path,
-                metadata.is_valid_dataset(self.transform_path)]):
-            operations.delete_dataset(self.transform_path, log_level=None)
-        # Extract to a new dataset.
+        LOG.info("Start: Extract %s.", extract_path)
         self.transform_path = operations.copy_dataset(
             dataset_path=extract_path,
             output_path=helpers.unique_temp_dataset_path('extract'),
             dataset_where_sql=extract_where_sql, schema_only=schema_only,
             log_level=None)
-        helpers.log_line('end', meta['description'])
+        LOG.info("End: Extract.")
         return self.transform_path
 
     def load(self, load_path, load_where_sql=None, preserve_features=False):
-        """Load features from transform workspace to the load-dataset."""
-        meta = {'description': "Load {}.".format(load_path)}
-        helpers.log_line('start', meta['description'])
+        """Load features from transform- to load-dataset."""
+        LOG.info("Start: Load %s.", load_path)
         if metadata.is_valid_dataset(load_path):
             # Load to an existing dataset.
-            # Unless preserving features, initialize the target dataset.
+            # Unless preserving features, initialize target dataset.
             if not preserve_features:
                 operations.delete_features(load_path, log_level=None)
             operations.insert_features_from_path(
@@ -68,27 +62,30 @@ class ArcETL(object):
             operations.copy_dataset(
                 dataset_path=self.transform_path, output_path=load_path,
                 dataset_where_sql=load_where_sql, log_level=None)
-        helpers.log_line('end', meta['description'])
+        LOG.info("End: Load.")
         return load_path
 
     def make_asssertion(self, assertion_name, **kwargs):
         """Check whether an assertion is valid or not."""
         raise NotImplementedError
 
-    def transform(self, transform_name, **kwargs):
+    def transform(self, transformation, **kwargs):
         """Run transform operation as defined in the workspace."""
-        transform_function = getattr(operations, transform_name)
-        # Unless otherwise stated, dataset path is self.transform path.
+        if isinstance(transformation, str):
+            transformation = getattr(operations, transformation)
+        # Unless otherwise stated, dataset path is self.transform_path.
         if 'dataset_path' not in kwargs:
             kwargs['dataset_path'] = self.transform_path
         # Add output_path to kwargs if needed.
-        if 'output_path' in inspect.getargspec(transform_function).args:
+        if all([
+                'output_path' in funcsigs.signature(transformation).parameters,
+                'output_path' not in kwargs]):
             kwargs['output_path'] = (
-                helpers.unique_temp_dataset_path(transform_name))
-        result = transform_function(**kwargs)
+                helpers.unique_temp_dataset_path(transformation.__name__))
+        result = transformation(**kwargs)
         # If there's a new output, replace old transform.
         if 'output_path' in kwargs:
             if metadata.is_valid_dataset(self.transform_path):
-                operations.delete_dataset(self.transform_path)
+                operations.delete_dataset(self.transform_path, log_level=None)
             self.transform_path = result
         return result
