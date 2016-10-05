2ccc2957e81b8cf61f4122b45f27af60e4a3ec17
Jacob Blair <jblair@lcog.org>
2016-10-05 10:56:39 -0700

    Create analyze submodule.

---
+++ arcetl/analyze.py
@@ -0,0 +1,10 @@
+# -*- coding=utf-8 -*-
+"""Analysis result operations."""
+import logging
+
+from arcetl._analyze import (  # pylint: disable=unused-import
+    near_features_as_dicts
+    )
+
+
+LOG = logging.getLogger(__name__)
