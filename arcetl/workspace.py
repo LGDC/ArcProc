094007dba8c9d13a526e8abc073b5e929998f380
Jacob Blair <jblair@lcog.org>
2016-11-02 16:54:06 -0700

    Rewrite workspace.dataset_names to use arcpy.da.Walk.
    Changed some kwargs to be more universal & Pythonic.
    
    Added workspace.dataset_paths.

--- arcetl/workspace.py
+++ arcetl/workspace.py
@@ -2,7 +2,15 @@
 """Workspace operations."""
 
 from arcetl._workspace import (
-    build_locator, build_network, compress, create_file_geodatabase,
-    create_geodatabase_xml_backup, dataset_names, domain_metadata, execute_sql,
-    is_valid, metadata
+    build_locator,
+    build_network,
+    compress,
+    create_file_geodatabase,
+    create_geodatabase_xml_backup,
+    dataset_names,
+    dataset_paths,
+    domain_metadata,
+    execute_sql,
+    is_valid,
+    metadata,
     )
