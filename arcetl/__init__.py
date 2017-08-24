a1cc7b57c24c0b3f418beab216ec52c894359a33
Jacob Blair <jblair@lcog.org>
2017-08-24 14:05:40 -0700

    Add arcpy package to __init__ for pass-through use.

--- arcetl/__init__.py
+++ arcetl/__init__.py
@@ -1,22 +1,35 @@
 """ETL framework library based on ArcGIS/ArcPy."""
 
 from arcetl import arcobj
-from arcetl.arcobj import (ArcExtension, DatasetView, TempDatasetCopy,
-                           spatial_reference_metadata)
+from arcetl.arcobj import (
+    ArcExtension,
+    DatasetView,
+    TempDatasetCopy,
+    spatial_reference_metadata,
+    )
 from arcetl import attributes
 from arcetl import combo
 from arcetl import convert
 from arcetl import dataset
 from arcetl import etl
+from arcetl.etl import (
+    ArcETL,
+    )
 from arcetl import features
 from arcetl import geoset
+from arcetl import helpers
+from arcetl.helpers import (
+    sexagesimal_angle_to_decimal,
+    unique_ids,
+    unique_name,
+    unique_temp_dataset_path,
+    )
 from arcetl import network
 from arcetl import proximity
 from arcetl import services
-from arcetl.etl import ArcETL
-from arcetl.helpers import (sexagesimal_angle_to_decimal, unique_ids,
-                            unique_name, unique_temp_dataset_path)
+# Imported here to enable pass-through access to ArcPy objects.
+import arcpy
 
 
-__all__ = []
+__all__ = ()
 __version__ = '2016.11'
