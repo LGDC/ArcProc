11200f40fb2d5785f2e54ea8e796b0cf69fd6978
Jacob Blair <jblair@lcog.org>
2018-04-30 15:07:48 -0700

    Formatting style: black.

--- arcetl/__init__.py
+++ arcetl/__init__.py
@@ -2,35 +2,26 @@
 
 from arcetl import arcobj
 from arcetl.arcobj import (
-    ArcExtension,
-    DatasetView,
-    Editor,
-    TempDatasetCopy,
-    spatial_reference_metadata,
-    )
+    ArcExtension, DatasetView, Editor, TempDatasetCopy, spatial_reference_metadata
+)
 from arcetl import attributes
 from arcetl import combo
 from arcetl import convert
 from arcetl import dataset
 from arcetl import diff
 from arcetl import etl
-from arcetl.etl import (
-    ArcETL,
-    )
+from arcetl.etl import ArcETL
 from arcetl import features
 from arcetl import geometry
 from arcetl import geoset
 from arcetl import helpers
 from arcetl.helpers import (
-    contain,
-    freeze_values,
-    unique_ids,
-    unique_name,
-    unique_path,
-    )
+    contain, freeze_values, unique_ids, unique_name, unique_path
+)
 from arcetl import network
 from arcetl import proximity
 from arcetl import services
+
 # Imported here to enable pass-through access to ArcPy objects.
 import arcpy
 
