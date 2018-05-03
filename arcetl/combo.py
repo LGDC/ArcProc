4b22ed59c479199636aa3fd32e64cc59da302503
Jacob Blair <jblair@lcog.org>
2018-05-03 13:36:11 -0700

    Swtich adjust_for_shapefile to use cursor directly, rather than attributes.update_by_function.

--- arcetl/combo.py
+++ arcetl/combo.py
@@ -2,8 +2,9 @@
 import datetime
 import logging
 
+import arcpy
+
 from arcetl import arcobj
-from arcetl import attributes
 from arcetl.helpers import leveled_logger
 
 
@@ -14,10 +15,10 @@ def adjust_for_shapefile(dataset_path, **kwargs):
     """Adjust features to meet shapefile requirements.
 
     Note:
-        Shapefiles cannot have null-values. Nulls will be replaced with the
-            values provided in the null replacement keyword arguments.
-        Shapefiles only have dates in the date/datetime field. Times will be
-            truncated in the adjustment.
+        Shapefiles cannot have null-values. Nulls will be replaced with the values
+            provided in the null replacement keyword arguments.
+        Shapefiles only have dates in the date/datetime field. Times will be truncated
+            in the adjustment.
 
     Args:
         dataset_path (str): Path of the dataset.
@@ -25,67 +26,64 @@ def adjust_for_shapefile(dataset_path, **kwargs):
 
     Keyword Args:
         dataset_where_sql (str): SQL where-clause for dataset subselection.
-        date_null_replacement (datetime.date): Replacement value for
-            null-values in date fields. Defaults to datetime.date.min.
-        integer_null_replacement (int): Replacement value for null-values in
-            integer fields. Defaults to 0.
-        log_level (str): Level to log the function at. Defaults to 'info'.
-        numeric_null_replacement (float): Replacement value for null-values
-            in numeric fields. Defaults to 0.0.
-        string_null_replacement (str): Replacement value for null-values in
-            string fields. Defaults to ''.
+        date_null_replacement (datetime.date): Replacement value for null-values in
+            date fields. Default is datetime.date.min.
+        integer_null_replacement (int): Replacement value for null-values in integer
+            fields. Default is 0.
+        numeric_null_replacement (float): Replacement value for null-values in numeric
+            fields. Default is 0.0.
+        string_null_replacement (str): Replacement value for null-values in string
+            fields. Default is ''.
+        use_edit_session (bool): Flag to perform updates in an edit session. Default is
+            False.
+        log_level (str): Level to log the function at. Default is 'info'.
 
     Returns:
         str: Path of the adjusted dataset.
+
     """
+    kwargs.setdefault('dataset_where_sql')
+    kwargs.setdefault('use_edit_session', False)
+    shp_type_replace_val = {
+        'date': kwargs.get('date_null_replacement', datetime.date.min),
+        'double': kwargs.get('numeric_null_replacement', 0.0),
+        'single': kwargs.get('numeric_null_replacement', 0.0),
+        'integer': kwargs.get('integer_null_replacement', 0),
+        'smallinteger': kwargs.get('integer_null_replacement', 0),
+        'string': kwargs.get('string_null_replacement', ''),
+        # Shapefile loader handles these types.
+        # 'geometry', 'oid',
+    }
     log = leveled_logger(LOG, kwargs.get('log_level', 'info'))
     log("Start: Adjust features for shapefile output in %s.", dataset_path)
-    shp_field_convert_types = (
-        'date', 'double', 'single', 'integer', 'smallinteger', 'string',
-        # Shapefile loader handles these types.
-        # 'Geometry', 'OID',
-        # Invalid shapefile types.
-        # 'Blob', 'GlobalID', 'Guid', 'Raster',
-        )
-    def shp_value_convertor_factory(field_type):
-        """Return value-convertor function for shapefile-valid values."""
-        def _date_convertor(value):
-            if value is None:
-                value = kwargs.get('date_null_replacement', datetime.date.min)
+    meta = {'dataset': arcobj.dataset_metadata(dataset_path)}
+    session = arcobj.Editor(
+        meta['dataset']['workspace_path'], kwargs['use_edit_session']
+    )
+    cursor_kwargs = {
+        'in_table': dataset_path, 'where_clause': kwargs['dataset_where_sql']
+    }
+    with session:
+        for field in meta['dataset']['user_fields']:
+            if field['type'].lower() not in shp_type_replace_val:
+                log("Skipping %s field: type cannot transfer to shapefile.")
+                continue
+
             else:
-                value = value.date()
-            return value
-        def _integer_convertor(value):
-            if value is None:
-                value = kwargs.get('integer_null_replacement', 0)
-            return value
-        def _numeric_convertor(value):
-            if value is None:
-                value = kwargs.get('numeric_null_replacement', 0.0)
-            return value
-        def _string_convertor(value):
-            if value is None:
-                value = kwargs.get('string_null_replacement', '')
-            return value
-        type_convertor = {
-            'date': _date_convertor,
-            'double': _numeric_convertor,
-            'integer': _integer_convertor,
-            'single': _numeric_convertor,
-            'smallinteger': _integer_convertor,
-            'string': _string_convertor,
-            }
-        return type_convertor[field_type.lower()]
-    for field in arcobj.dataset_metadata(dataset_path)['fields']:
-        # Ignore fields that conversion won't affect in copying to shapefiles.
-        if field['type'].lower() not in shp_field_convert_types:
-            continue
-        LOG.info("Adjusting values in %s for shapefile output.", field['name'])
-        attributes.update_by_function(
-            dataset_path, field['name'],
-            function=shp_value_convertor_factory(field['type']),
-            dataset_where_sql=kwargs.get('dataset_where_sql'),
-            log_level=None
-            )
+                log("Adjusting values in %s field.", field['name'])
+            cursor = arcpy.da.UpdateCursor(field_names=[field['name']], **cursor_kwargs)
+            with cursor:
+                for old_val, in cursor:
+                    if old_val is None:
+                        new_val = shp_type_replace_val[field['type'].lower()]
+                    else:
+                        new_val = old_val
+                    if old_val != new_val:
+                        try:
+                            cursor.updateRow([new_val])
+                        except RuntimeError:
+                            LOG.error("Offending value is %s", new_val)
+                            raise RuntimeError
+
     log("End: Adjust.")
     return dataset_path
