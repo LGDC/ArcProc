3770bb25a001c943ff2a492d2597fc1f4be1f0d0
Jacob Blair <jblair@lcog.org>
2018-05-21 15:21:35 -0700

    Formatting: Black.

--- arcetl/helpers.py
+++ arcetl/helpers.py
@@ -22,8 +22,8 @@ def contain(obj, nonetypes_as_empty=True):
 
     Args:
         obj: Any object, collection or otherwise.
-        nontypes_as_empty (bool): True if NoneTypes treated as an empty
-            collection, otherwise False.
+        nontypes_as_empty (bool): If True `None` will be  treated as an empty
+            collection; if False, they will be treated as an object to generate.
 
     Yields:
         obj or its contents.
@@ -31,17 +31,19 @@ def contain(obj, nonetypes_as_empty=True):
     """
     if nonetypes_as_empty and obj is None:
         return
+
     if inspect.isgeneratorfunction(obj):
         obj = obj()
     if isinstance(obj, Iterable) and not isinstance(obj, basestring):
         for i in obj:
             yield i
+
     else:
         yield obj
 
 
 def freeze_values(*values):
-    """Generator for iterable with bytearrays converted to bytes.
+    """Generate 'frozen' versions of mutable objects.
 
     Currently only freezes bytearrays to bytes.
 
@@ -49,19 +51,20 @@ def freeze_values(*values):
         values: Values to return.
 
     Yields:
-        object: If a value is mutable, will yield value as immutable type of the
-            value. Otherwise will yield as original value/type.
+        object: If a value is mutable, will yield value as immutable type of the value.
+            Otherwise will yield as original value/type.
 
     """
     for val in values:
         if isinstance(val, bytearray):
             yield bytes(val)
+
         else:
             yield val
 
 
 def leveled_logger(logger, level_repr=None):
-    """Return function to log into logger at the given level.
+    """Return logger function to log at the given level.
 
     Args:
         logger (logging.Logger): Logger to log to.
@@ -71,8 +74,10 @@ def leveled_logger(logger, level_repr=None):
         function.
 
     """
+
     def _logger(msg, *args, **kwargs):
         return logger.log(log_level(level_repr), msg, *args, **kwargs)
+
     return _logger
 
 
@@ -86,9 +91,14 @@ def log_level(level_repr=None):
         int: Logging module level.
 
     """
-    level = {None: 0, 'debug': logging.DEBUG, 'info': logging.INFO,
-             'warning': logging.WARNING, 'error': logging.ERROR,
-             'critical': logging.CRITICAL}
+    level = {
+        None: 0,
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL,
+    }
     if level_repr in level.values():
         result = level_repr
     elif level_repr is None:
@@ -97,6 +107,7 @@ def log_level(level_repr=None):
         result = level[level_repr.lower()]
     else:
         raise RuntimeError("level_repr invalid.")
+
     return result
 
 
@@ -116,6 +127,7 @@ def property_value(item, property_transform_map, *properties):
     """
     if item is None:
         return None
+
     current_val = item
     for prop in properties:
         # Replace stand-ins with ordered properties.
@@ -133,34 +145,39 @@ def unique_ids(data_type=uuid.UUID, string_length=4):
 
     Args:
         data_type: Type object to create unique IDs as.
-        string_length (int): Length to make unique IDs of type string.
-            Ignored if data_type is not a stringtype.
+        string_length (int): Length to make unique IDs of type string. Ignored if
+            data_type is not a string type.
 
     Yields:
         Unique ID.
 
     """
-    if data_type in (float, int):
+    if data_type in [float, int]:
         # Skip 0 (problematic - some processing functions use 0 for null).
         unique_id = data_type(1)
         while True:
             yield unique_id
+
             unique_id += 1
-    elif data_type in (uuid.UUID,):
+    elif data_type in [uuid.UUID]:
         while True:
             yield uuid.uuid4()
-    elif data_type in (str,):
+
+    elif data_type in [str]:
         seed = string.ascii_letters + string.digits
         used_ids = set()
         while True:
             unique_id = ''.join(random.choice(seed) for _ in range(string_length))
             if unique_id in used_ids:
                 continue
+
             yield unique_id
+
+            used_ids.add(unique_id)
     else:
         raise NotImplementedError(
             "Unique IDs for {} type not implemented.".format(data_type)
-            )
+        )
 
 
 def unique_name(prefix='', suffix='', unique_length=4, allow_initial_digit=True):
@@ -170,8 +187,8 @@ def unique_name(prefix='', suffix='', unique_length=4, allow_initial_digit=True)
         prefix (str): String to insert before the unique part of the name.
         suffix (str): String to append after the unique part of the name.
         unique_length (int): Number of unique characters to generate.
-        allow_initial_number (bool): Flag indicating whether to let the
-            initial character be a number. Default is True.
+        allow_initial_number (bool): Flag indicating whether to let the initial
+            character be a number. Default is True.
 
     Returns:
         str: Unique name.
@@ -196,6 +213,5 @@ def unique_path(prefix='', suffix='', unique_length=4, workspace_path='in_memory
         str: Path of the created dataset.
 
     """
-    name = unique_name(prefix, suffix, unique_length,
-                       allow_initial_digit=False)
+    name = unique_name(prefix, suffix, unique_length, allow_initial_digit=False)
     return os.path.join(workspace_path, name)
