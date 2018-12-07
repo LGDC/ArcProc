48724366c67e94126364c63e9cc56971f4eb3d8c
Jacob Blair <jblair@lcog.org>
2018-12-07 15:53:42 -0800

    Init codestyle: Black.

--- arcetl/__init__.py
+++ arcetl/__init__.py
@@ -1,8 +1,13 @@
 """ETL framework library based on ArcGIS/ArcPy."""
 
+# pylint: disable=relative-beyond-top-level, unused-import
 from arcetl import arcobj
 from arcetl.arcobj import (
-    ArcExtension, DatasetView, Editor, TempDatasetCopy, spatial_reference_metadata
+    ArcExtension,
+    DatasetView,
+    Editor,
+    TempDatasetCopy,
+    spatial_reference_metadata,
 )
 from arcetl import attributes
 from arcetl import combo
@@ -15,16 +20,14 @@ from arcetl import features
 from arcetl import geometry
 from arcetl import geoset
 from arcetl import helpers
-from arcetl.helpers import (
-    contain, freeze_values, unique_ids, unique_name, unique_path
-)
+from arcetl.helpers import contain, freeze_values, unique_ids, unique_name, unique_path
 from arcetl import network
 from arcetl import proximity
 from arcetl import services
 
-# Imported here to enable pass-through access to ArcPy objects.
-import arcpy
+# pylint: enable=relative-beyond-top-level, unused-import
 
 
-__all__ = ()
-__version__ = '2016.11'
+__all__ = []
+##TODO: Replace YYYY.MM with semantic versioning.
+__version__ = "2016.11"
