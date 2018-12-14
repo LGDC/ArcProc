8411085094114526702a54dc09094e8fa3e0684b
Jacob Blair <jblair@lcog.org>
2018-12-14 09:17:12 -0800

    Removing diff submodule (for now). Does not work well (memory/speed) with relatively large datasets.

--- arcetl/__init__.py
+++ arcetl/__init__.py
@@ -13,7 +13,6 @@ from arcetl import attributes
 from arcetl import combo
 from arcetl import convert
 from arcetl import dataset
-from arcetl import diff
 from arcetl import etl
 from arcetl.etl import ArcETL
 from arcetl import features
