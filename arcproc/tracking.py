69306a18350f624fd2b94abd7e7044b521c801fc
Jacob Blair <jblair@lcog.org>
2021-05-25 15:58:44 -0700

    Convert id_field_name -> id_field_names.

--- arcproc/tracking.py
+++ arcproc/tracking.py
@@ -10,14 +10,14 @@ import arcpy
 from arcproc import arcobj
 from arcproc import attributes
 from arcproc import features
-from arcproc.helpers import log_entity_states, same_value
+from arcproc.helpers import contain, log_entity_states, same_value
 
 
 LOG = logging.getLogger(__name__)
 """logging.Logger: Module-level logger."""
 
 
-def consolidate_rows(dataset_path, field_name, id_field_name, **kwargs):
+def consolidate_rows(dataset_path, field_name, id_field_names, **kwargs):
     """Consolidate tracking dataset rows where the value did not actually change.
 
     Useful for quick-loaded point-in-time values, or for processing hand-altered rows.
@@ -25,7 +25,7 @@ def consolidate_rows(dataset_path, field_name, id_field_name, **kwargs):
     Args:
         dataset_path (str): Path of tracking dataset.
         field_name (str): Name of tracked field.
-        id_field_name (str): Name of ID field.
+        id_field_names (iter): Field names used to identify a feature.
         **kwargs: Arbitrary keyword arguments. See below.
 
     Keyword Args:
@@ -42,39 +42,35 @@ def consolidate_rows(dataset_path, field_name, id_field_name, **kwargs):
     """
     level = kwargs.get("log_level", logging.INFO)
     LOG.log(level, "Start: Consolidate tracking rows in `%s`.", dataset_path)
-    for key in ["date_initiated", "date_expired"]:
-        if not kwargs.get(key + "_field_name"):
-            kwargs[key + "_field_name"] = key
-    field_names = [
-        id_field_name,
-        kwargs["date_initiated_field_name"],
-        kwargs["date_expired_field_name"],
-        field_name,
-    ]
+    keys = {
+        "id": list(contain(id_field_names)),
+        "date": [
+            kwargs.get(key + "_field_name", key)
+            for key in ["date_initiated", "date_expired"]
+        ],
+    }
+    keys["row"] = keys["id"] + keys["date"] + [field_name]
     id_rows = defaultdict(list)
-    for row in attributes.as_dicts(dataset_path, field_names):
-        id_rows[row[id_field_name]].append(row)
+    for row in attributes.as_dicts(dataset_path, field_names=keys["row"]):
+        id_ = tuple(row[key] for key in keys["id"])
+        id_rows[id_].append(row)
     for id_ in list(id_rows):
-        rows = sorted(id_rows[id_], key=itemgetter(kwargs["date_initiated_field_name"]))
+        rows = sorted(id_rows[id_], key=itemgetter(keys["date"][0]))
         for i, row in enumerate(rows):
-            if i == 0 or row[kwargs["date_initiated_field_name"]] is None:
+            if i == 0 or row[keys["date"][0]] is None:
                 continue
 
             previous_row = rows[i - 1]
             if same_value(row[field_name], previous_row[field_name]):
                 # Move previous row date initiated to current row & clear from previous.
-                row[kwargs["date_initiated_field_name"]] = previous_row[
-                    kwargs["date_initiated_field_name"]
-                ]
-                previous_row[kwargs["date_initiated_field_name"]] = None
-        id_rows[id_] = [
-            row for row in rows if row[kwargs["date_initiated_field_name"]] is not None
-        ]
+                row[keys["date"][0]] = previous_row[keys["date"][0]]
+                previous_row[keys["date"][0]] = None
+        id_rows[id_] = [row for row in rows if row[keys["date"][0]] is not None]
     states = features.update_from_dicts(
         dataset_path,
         update_features=chain(*id_rows.values()),
-        id_field_names=field_names[:2],
-        field_names=field_names,
+        id_field_names=keys["id"],
+        field_names=keys["row"],
         use_edit_session=kwargs.get("use_edit_session", False),
         log_level=logging.DEBUG,
     )
@@ -83,13 +79,13 @@ def consolidate_rows(dataset_path, field_name, id_field_name, **kwargs):
     return states
 
 
-def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwargs):
+def update_rows(dataset_path, field_name, id_field_names, cmp_dataset_path, **kwargs):
     """Add field value changes to tracking dataset from comparison dataset.
 
     Args:
         dataset_path (str): Path of tracking dataset.
         field_name (str): Name of tracked field.
-        id_field_name (str): Name of ID field.
+        id_field_names (iter): Field names used to identify a feature.
         cmp_dataset_path (str): Path of comparison dataset.
         **kwargs: Arbitrary keyword arguments. See below.
 
@@ -98,8 +94,8 @@ def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwa
             Default is date of execution.
         cmp_field_name (str): Name of tracked field in comparison dataset, if different
             from field_name.
-        cmp_id_field_name (str): Name of ID field in comparison dataset, if different
-            from id_field_name.
+        cmp_id_field_names (iter): Field names used to identify a feature in comparison
+            dataset, if different from id_field_name.
         date_initiated_field_name (str): Name of tracking-row-inititated date field.
             Default is "date_initiated".
         date_expired_field_name (str): Name of tracking-row-expired date field. Default
@@ -120,21 +116,26 @@ def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwa
     )
     if not kwargs.get("cmp_date"):
         kwargs["cmp_date"] = datetime.date.today()
-    for key in ["date_initiated", "date_expired"]:
-        if not kwargs.get(key + "_field_name"):
-            kwargs[key + "_field_name"] = key
-    current_where_sql = "{date_expired_field_name} IS NULL".format(**kwargs)
+    keys = {
+        "id": list(contain(id_field_names)),
+        "cmp_id": list(contain(kwargs.get("cmp_id_field_names", id_field_names))),
+        "date": [
+            kwargs.get(key + "_field_name", key)
+            for key in ["date_initiated", "date_expired"]
+        ],
+    }
+    current_where_sql = "{} IS NULL".format(keys["date"][1])
     meta = {"dataset": arcobj.dataset_metadata(dataset_path)}
     id_value = {}
     id_value["current"] = attributes.id_values_map(
         dataset_path,
-        id_field_names=[id_field_name],
+        id_field_names=keys["id"],
         field_names=[field_name],
         dataset_where_clause=current_where_sql,
     )
     id_value["cmp"] = attributes.id_values_map(
         cmp_dataset_path,
-        id_field_names=[kwargs.get("cmp_id_field_name", id_field_name)],
+        id_field_names=keys["cmp_id"],
         field_names=[kwargs.get("cmp_field_name", field_name)],
     )
     ids = {
@@ -144,10 +145,10 @@ def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwa
     new_rows = []
     for id_, value in id_value["cmp"].items():
         if id_ not in id_value["current"]:
-            new_rows.append((id_, value, kwargs["cmp_date"]))
+            new_rows.append(id_ + (value, kwargs["cmp_date"]))
         elif not same_value(value, id_value["current"][id_]):
             ids["changing"].add(id_)
-            new_rows.append((id_, value, kwargs["cmp_date"]))
+            new_rows.append(id_ + (value, kwargs["cmp_date"]))
     states = Counter()
     # Could replace cursor with features.update_from_iters if that function adds a
     # dataset_where_sql keyword argument.
@@ -156,12 +157,14 @@ def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwa
     )
     cursor = arcpy.da.UpdateCursor(
         dataset_path,
-        field_names=[id_field_name, field_name, kwargs["date_expired_field_name"]],
+        field_names=keys["id"] + [field_name, keys["date"][1]],
+        where_clause=current_where_sql,
     )
     with session, cursor:
-        for id_, value, _ in cursor:
+        for row in cursor:
+            id_ = tuple(row[: len(keys["id"])])
             if id_ in ids["changing"] or id_ in ids["expiring"]:
-                cursor.updateRow((id_, value, kwargs["cmp_date"]))
+                cursor.updateRow((id_, row[-2], kwargs["cmp_date"]))
                 states["altered"] += 1
             else:
                 states["unchanged"] += 1
@@ -169,11 +172,7 @@ def update_rows(dataset_path, field_name, id_field_name, cmp_dataset_path, **kwa
         features.insert_from_iters(
             dataset_path,
             insert_features=new_rows,
-            field_names=[
-                id_field_name,
-                field_name,
-                kwargs["date_initiated_field_name"],
-            ],
+            field_names=keys["id"] + [field_name, keys["date"][0]],
             use_edit_session=kwargs.get("use_edit_session", False),
             log_level=logging.DEBUG,
         )
