f2101f332d7cf7a23e5e10b9aef873e6a3903510
Jacob Blair <jblairpdx@gmail.com>
2022-02-04 10:22:30 -0800

    Refactor: Annotations, docstrings, organization, cleanup.

--- arcproc/exceptions.py
+++ arcproc/exceptions.py
@@ -1,56 +1,57 @@
 """Exceptions subpackage."""
+from pathlib import Path
 
 
 class DatasetNotFoundError(Exception):
-    """Exception raised when dataset not found at given path.
+    """Exception raised when dataset not found at given path."""
 
-    Attributes:
-        dataset_path: Path to the not-found dataset.
-        message: Explanation of the error.
-    """
-
-    def __init__(self, dataset_path, message="Dataset not found at path"):
-        self.dataset_path = dataset_path
-        self.message = message
+    def __init__(
+        self, dataset_path: Path, message: str = "Dataset not found at path"
+    ) -> None:
+        self.dataset_path: Path = dataset_path
+        """Path to the not-found dataset."""
+        self.message: str = message
+        """Explanation of the error."""
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.dataset_path} -> {self.message}"
 
 
 class DomainNotFoundError(Exception):
-    """Exception raised when domain not found in given geodatabase.
+    """Exception raised when domain not found in given geodatabase."""
 
-    Attributes:
-        geodatabase_path: Path to geodatabase expected to have the domain.
-        domain_name: Name of the not-found domain.
-        message: Explanation of the error.
-    """
-
-    def __init__(self, geodatabase_path, domain_name, message="Domain not found"):
-        self.geodatabase_path = geodatabase_path
-        self.domain_name = domain_name
+    def __init__(
+        self,
+        geodatabase_path: Path,
+        domain_name: str,
+        message: str = "Domain not found",
+    ) -> None:
+        self.geodatabase_path: Path = geodatabase_path
+        """Path to geodatabase expected to have the domain."""
+        self.domain_name: str = domain_name
+        """Name of the not-found domain."""
         self.message = message
+        """Explanation of the error."""
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.domain_name} in {self.geodatabase_path} -> {self.message}"
 
 
 class FieldNotFoundError(Exception):
-    """Exception raised when field not found on given dataset.
+    """Exception raised when field not found on given dataset."""
 
-    Attributes:
-        dataset_path: Path to dataset expected to carry the field.
-        field_name: Name of the not-found field.
-        message: Explanation of the error.
-    """
-
-    def __init__(self, dataset_path, field_name, message="Field not found"):
+    def __init__(
+        self, dataset_path: Path, field_name: str, message: str = "Field not found",
+    ) -> None:
         self.dataset_path = dataset_path
+        """Path to dataset expected to carry the field."""
         self.field_name = field_name
+        """Name of the not-found field."""
         self.message = message
+        """Explanation of the error."""
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.field_name} on {self.dataset_path} -> {self.message}"
