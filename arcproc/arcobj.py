b2f14c9befe80b58ee3fd2a3f3fb419b993fddbd
Jacob Blair <jblairpdx@gmail.com>
2022-02-03 16:17:08 -0800

    Refactor: Remove `linear_unit` - never used.

--- arcproc/arcobj.py
+++ arcproc/arcobj.py
@@ -1,37 +1,11 @@
 """Interfaces for ArcObjects."""
 import logging
 
-import arcpy
-
-from arcproc import geometry
 from arcproc.metadata import SpatialReference
 
 
 LOG = logging.getLogger(__name__)
 """logging.Logger: Module-level logger."""
-
-arcpy.SetLogHistory(False)
-
-
-def linear_unit(measure_string, spatial_reference_item):
-    """Return linear unit of measure in reference units from string.
-
-    Args:
-        unit_string (str): String description of linear unit of measure.
-        spatial_reference_item: Item from which the linear unit"s spatial
-            reference will be derived.
-
-    Returns:
-        float: Unit of measure in spatial reference"s units.
-
-    """
-    str_measure, str_unit = measure_string.split(" ")
-    reference_unit = getattr(
-        SpatialReference(spatial_reference_item), "linear_unit", "Unknown"
-    )
-    meter_measure = float(str_measure) * geometry.RATIO["meter"][str_unit.lower()]
-    measure = meter_measure / geometry.RATIO["meter"][reference_unit.lower()]
-    return measure
 
 
 def linear_unit_string(measure, spatial_reference_item):
