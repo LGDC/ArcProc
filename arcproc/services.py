fc66ac12fd4dde455c9cfd651c0b64737517f78a
Jacob Blair <jblairpdx@gmail.com>
2022-02-28 11:53:59 -0800

    Refactor: Annotations, docstrings, organization, cleanup.

--- arcproc/services.py
+++ arcproc/services.py
@@ -1,53 +1,59 @@
 """ArcGIS Server service operations."""
 import logging
+from pathlib import Path
+from typing import Iterable, Iterator, Optional, Union
 
 import arcgis
 
 import arcpy
 
-from arcproc.helpers import contain
 from arcproc.metadata import SpatialReference
 
 
-LOG = logging.getLogger(__name__)
-"""logging.Logger: Module-level logger."""
+LOG: logging.Logger = logging.getLogger(__name__)
+"""Module-level logger."""
+
+arcpy.SetLogHistory(False)
 
 
-def as_dicts(url, field_names=None, **kwargs):
+def as_dicts(
+    url: str,
+    field_names: Optional[Iterable[str]] = None,
+    service_where_sql: Optional[str] = None,
+    include_geometry: bool = True,
+    spatial_reference_item: Optional[
+        Union[SpatialReference, int, arcpy.Geometry, arcpy.SpatialReference, Path, str]
+    ] = None,
+) -> Iterator[dict]:
     """Generate mappings of feature attribute name to value.
 
     Notes:
         Use ArcPy cursor token names for object IDs and geometry objects/properties.
 
     Args:
-        url (str): URL for the service endpoint.
-        field_names (iter): Collection of field names. Names will be the keys in the
-            dictionary mapping to their values. If value is None, all attributes fields
-            will be used.
-        **kwargs: Arbitrary keyword arguments. See below.
-
-    Keyword Args:
-        service_where_sql (str): SQL where-clause for service feature subselection.
+        url: URL for the service endpoint.
+        field_names: Collection of field names to include in dictionary. If set to None,
+            all fields will be included. Do not include geometry field; use
+            `include_geometry` to have added to dictionary.
+        include_geometry: Add geometry attribute to dictionary under "SHAPE@" key if
+            True.
+        service_where_sql: SQL where-clause for service subselection.
         spatial_reference_item: Item from which the spatial reference of the output
-            geometry will be derived.
-
-    Yields:
-        dict
+            geometry will be derived. If set to None, will use spatial reference of the
+            service.
     """
-    if field_names is None:
-        field_names = "*"
-    else:
-        field_names = list(contain(field_names))
-    wkid = SpatialReference(kwargs.get("spatial_reference_item")).wkid
+    # `spatial_reference_item = None` will return instance with wkid being None.
+    wkid = SpatialReference(spatial_reference_item).wkid
     feature_layer = arcgis.features.FeatureLayer(url)
     feature_set = feature_layer.query(
-        where=kwargs.get("service_where_sql", "1=1"),
-        out_fields=field_names,
+        where=service_where_sql if service_where_sql else "1=1",
+        out_fields="*" if field_names is None else list(field_names),
         out_sr=wkid,
     )
     for feature in feature_set.features:
         feature_dict = feature.attributes
-        if "spatialReference" not in feature.geometry:
-            feature.geometry["spatialReference"] = {"wkid": wkid}
-        feature_dict["SHAPE@"] = arcpy.AsShape(feature.geometry, esri_json=True)
+        if include_geometry:
+            if "spatialReference" not in feature.geometry:
+                feature.geometry["spatialReference"] = {"wkid": wkid}
+            feature_dict["SHAPE@"] = arcpy.AsShape(feature.geometry, esri_json=True)
         yield feature_dict
